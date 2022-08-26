b13ab2eb4a2bff8b8260e76b8459da86d36be14d
Robert <robert.smol@stereoit.com>
2022-08-26 11:23:45 +0200

    Updated k6 script to perform a story

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, group, check } from 'k6';
+
+const options = {
+    vus: 10,
+    duration: '60s',
+};
+const SLEEP_DURATION = 0.1;
+
+const baseUrl = __ENV.APP_HOST;
 
 export default function () {
-    // http.get('https://localhost:8080');
-    // console.log("APP_HOST=", process.env.APP_HOST)
-    const res = http.get(`http://${__ENV.APP_HOST}/`);
-    // http.get('cool-water-1296.fly.dev'); // make this env var
+    const res = http.get(`http://${baseUrl}/`);
     sleep(1);
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    // const login_response = login();
+    // check(login_response, {
+    //     'is status 200': (r) => r.status === 200,
+    //     'is access token present': (r) => r.json().hasOwnProperty('accessToken'),
+    // })
+
+    // params['Auth'] = `Bearer ${login_response.json()['accessToken']}`;
+    sleep(SLEEP_DURATION);
+
+    group('Articles', (_) => {
+        params.tags.name = 'get-all-articles'
+        // Get all articles
+        const articles_response = http.get(
+            `${baseUrl}/articles`, params
+        );
+        check(articles_response, {
+            'is status 200': (r) => r.status === 200,
+        })
+        sleep(SLEEP_DURATION);
+
+
+        // Create new article
+        // Get article details
+        // Delete article
+    });
 }
 
 // export let options = {
@@ -18,3 +55,24 @@ export default function () {
 //         }
 //     }
 // }
+
+
+function login() {
+    const url = 'http://api.platform.com/login';
+
+    const payload = JSON.stringify({
+        email: 'robert.smol@2k.com',
+        password: 'PASSWORD',
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        tags: {
+            name: 'login',
+        }
+    };
+
+    return http.post(url, payload, params); 
+}
